517658ca9a817d23a262cfa7b73a3864771a7fd3
Chris Mo <cmochinski@gmail.com>
2021-07-28 10:21:28 -0500

    pushing result of vids 1 n 2 for practice

---
+++ arraysExample.js
@@ -0,0 +1,8 @@
+console.log( "Arrays Example" );
+
+let bagel = [ 'bacon', 'lettuce', 'tomato' ];
+//all STRINGS, inside square braces so system knows it's an array, all split by
+//commas and there's not trailing comma
+//DELINEATED by commas
+//DELINEATE = "describe or portray something precisely"
+//or "indicate the exact position of (a border or boundary)"
